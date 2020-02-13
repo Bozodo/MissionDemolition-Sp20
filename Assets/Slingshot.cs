b710c4467e8eb5a6a72ceeae16028cff7930057b
Bozodo <aquinojt.1@gmail.com>
2020-02-13 15:08:44 -0500

    Updated Slingshot.cs to instantiate Projectiles onMouseDown

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    //attributes help organize components in the Unity Inspector pane
+
+    //fields set in Unity Inspector pane
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    //fields set dynamically
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+
     private void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
     private void OnMouseEnter()
     {
@@ -21,5 +35,20 @@ public class Slingshot : MonoBehaviour
     {
         print("Slingshot: OnMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        //Mouse button is pressed down
+        aimingMode = true;
+
+        //Instantiate a projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+
+        //Start it at the launchPoint
+        projectile.transform.position = launchPos;
+
+        //Temporarily set projectile to isKinematic so we can move it around
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
